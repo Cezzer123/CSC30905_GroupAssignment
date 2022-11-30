22ed3076f6c8539d24c932b48b288331ad87bdcd
Cezzer123 <119027348+Cezzer123@users.noreply.github.com>
2022-11-30 15:14:16 +0800

    counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,20 +235,3 @@ submitBtn.addEventListener('click', ()=>{
     inputElm.value = '';
 
 })
-/**
-  
-   * Skills animation
-   */
-    let skilsContent = select('.skills-content');
-    if (skilsContent) {
-      new Waypoint({
-        element: skilsContent,
-        offset: '80%',
-        handler: function(direction) {
-          let progress = select('.progress .progress-bar', true);
-          progress.forEach((el) => {
-            el.style.width = el.getAttribute('aria-valuenow') + '%'
-          });
-        }
-      })
-    }
